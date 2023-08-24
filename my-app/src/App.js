3a89eaf7b4d08fcea647c5e0fd86b3b0388c893b
Davidtariq96 <samikendrickjnr@gmail.com>
2023-08-24 08:10:49 +0100

    created build folder for deployment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -33,10 +33,24 @@ class  App extends Component {
       <div className="App">
         {
            items?.map((user) => (
-            <div> 
-               <img src={user.picture.large} alt={user.name.first}/>
-                <div className="user">{user.name.first}</div>
+            <div className="container mt-5">
+            <div className="card shadow-lg">
+              <div className="row g-0">
+                <div className="col-md-4 bg-primary text-center d-flex align-items-center">
+                  <img src={user.picture.large} alt={user.name.first} className="img-fluid rounded-circle mx-auto" style={{ maxWidth: '80%', maxHeight: '80%' }} />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h2 className="card-title">{user.name.last} {user.name.first}</h2>
+                    <p className="card-text"><strong>name: </strong>{user.name.last} {user.name.first}</p>
+                    <p className="card-text"><strong>Email:</strong> {user.email}</p>
+                    <p className="card-text"><strong>Location:</strong> {user.location.country}</p>
+                    <p className="card-text"><strong>Age:</strong> {user.dob.age}</p>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
           ))
         }
       </div>
