1964a078d7789d50578d90d689df86d5c810b4bc
Davidtariq96 <samikendrickjnr@gmail.com>
2023-08-24 07:48:37 +0100

    fectching dynamic user data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {Component}from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Checking <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [],
+      loading: false
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://randomuser.me/api").then((response) => {
+      console.log(response);
+      response.json().then((data) => {
+          console.log(data.results);
+          this.setState({
+                  items: data.results,
+                  loading:true
+            })
+      });
+  });
+  }
+
+ render() {
+  var {items, loading} = this.state
+  if(!loading){
+    return <div> Loading... </div>
+  }else{
+    return (
+      <div className="App">
+        {
+           items?.map((user) => (
+            <div> 
+               <img src={user.picture.large} alt={user.name.first}/>
+                <div className="user">{user.name.first}</div>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+ } 
 }
 
 export default App;
